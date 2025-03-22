ebcb2ddcd1d722527e36c9a949f161f9f56aa9ef
Dusteleven <quinnkozik@gmail.com>
2025-03-22 13:35:52 -0500

    Update find_unmounted_disks.sh

--- template/0.8.2/ansible/cmn/find_unmounted_disks.sh
+++ template/0.8.2/ansible/cmn/find_unmounted_disks.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-find_unmounted_nvme_disks() {
-    lsblk -nr -o NAME,TYPE,SIZE,MOUNTPOINT | awk '
-    $2 == "disk" && 
-    $1 ~ /^nvme/ && 
-    (($3 ~ /G$/ && substr($3, 1, length($3)-1) + 0 >= 800) || 
-     ($3 ~ /T$/ && substr($3, 1, length($3)-1) + 0 >= 0.8)) && 
-    ($4 == "" || $4 ~ /^[[:space:]]*$/) && 
-    system("lsblk -nr -o TYPE /dev/" $1 " | grep -q part") != 0 {print $1}'
+# This script discovers unmounted, unpartitioned disks >=400GB,
+# prioritizes NVMe over others, and sorts by size descending.
+
+find_unmounted_disks() {
+  lsblk -nr -o NAME,TYPE,SIZE,MOUNTPOINT | awk '
+    $2 == "disk" &&
+    ($4 == "" || $4 ~ /^[[:space:]]*$/) &&
+    system("lsblk -nr -o TYPE /dev/" $1 " | grep -q part") != 0 {
+      size = $3
+      suffix = substr(size, length(size), 1)
+      base = substr(size, 1, length(size)-1) + 0
+      if (suffix == "T") base *= 1000
+      if (base >= 400) {
+        nvme = ($1 ~ /^nvme/) ? 1 : 0
+        print nvme, base, $1
+      }
+    }' | sort -k1,1nr -k2,2nr | awk '{print $3}'
 }
 
-find_unmounted_nvme_disks
+find_unmounted_disks
