827e5af5e90e8f26a692c307e3237c43444dab3f
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-05-28 10:34:14 +0200

    rm pnpm

--- cf-workers-template/contact-email/src/config.ts
+++
@@ -1,8 +0,0 @@
-export const fromEmail = 'noreply@slv.dev'
-export const bccEmail = 'labo@elsoul.nl'
-export const orgName = 'SLV Dev'
-export const logoUrl = 'https://storage.slv.dev/SLVLogoInvertHorizontal.png'
-
-export const allowedOrigin = ['https://web-ssg.slv.dev']
-
-export const devOrigin = ['http://localhost:4242']
--- website/slv-dev/next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-const appInfo = require('./appInfo')
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: `https://${appInfo.domain}`,
-  generateRobotsTxt: true,
-  exclude: ['/', '*.jpg', '*.png', '*.jpeg', '*.svg'],
-  outDir: './out'
-}
