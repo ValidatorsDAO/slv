c8c2c7904c73f244496f355e9986df940fd608e7
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-05-30 16:11:35 +0200

    update bot template

--- cmn/zod/bot.ts
+++ cmn/zod/bot.ts
@@ -4,6 +4,6 @@ export type BotTempType = 'geyser-typescript' | 'shreds-rust'
 export const BotTempTypeSchema = z.enum(['geyser-typescript', 'shreds-rust'])
 export const BotTempTypeArray = BotTempTypeSchema.options
 export const BotTempDLinkMap: Record<BotTempType, string> = {
-  'geyser-typescript': 'https://storage.slv.dev/template/geyser-ts2.tar.gz',
-  'shreds-rust': 'https://storage.slv.dev/template/shreds-rs2.tar.gz',
+  'geyser-typescript': 'https://storage.slv.dev/template/geyser-ts3.tar.gz',
+  'shreds-rust': 'https://storage.slv.dev/template/shreds-rs3.tar.gz',
 }
