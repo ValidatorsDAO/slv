011d5df8f9786b74da59a1467e10514d5b3359d5
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-06-14 01:08:40 +0200

    update mainnet validator verion - jito/firedancer/agave

--- cmn/zod/bot.ts
+++ cmn/zod/bot.ts
@@ -6,5 +6,5 @@ export const BotTempTypeArray = BotTempTypeSchema.options
 export const BotTempDLinkMap: Record<BotTempType, string> = {
   'geyser-typescript': 'https://storage.slv.dev/template/geyser-ts3.tar.gz',
   'shreds-rust': 'https://storage.slv.dev/template/shreds-rs3.tar.gz',
-  'shreds-ts': 'https://storage.slv.dev/template/shreds-ts.tar.gz',
+  'shreds-ts': 'https://storage.slv.dev/bin/latest/shreds-ts',
 }
