e08762546b99999f6b12852efb39f6098e85d2fb
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-06-27 15:07:47 +0200

    update slv bot init

--- cmn/zod/bot.ts
+++ cmn/zod/bot.ts
@@ -1,10 +1,11 @@
 import { z } from '@hono/zod-openapi'
 
-export type BotTempType = 'geyser-typescript' | 'shreds-rust' | 'shreds-ts'
-export const BotTempTypeSchema = z.enum(['geyser-typescript', 'shreds-rust', 'shreds-ts'])
+export type BotTempType = 'geyser-ts' | 'shreds-rust' | 'shreds-ts' | 'geyser-rust'
+export const BotTempTypeSchema = z.enum(['geyser-ts', 'geyser-rust', 'shreds-ts','shreds-rust' ])
 export const BotTempTypeArray = BotTempTypeSchema.options
 export const BotTempDLinkMap: Record<BotTempType, string> = {
-  'geyser-typescript': 'https://storage.slv.dev/template/geyser-ts3.tar.gz',
-  'shreds-rust': 'https://storage.slv.dev/template/shreds-rs3.tar.gz',
+  'geyser-ts': 'https://storage.slv.dev/bin/latest/geyser-ts',
+  'geyser-rust': 'https://storage.slv.dev/bin/latest/geyser-rs',
+  'shreds-rust': 'https://storage.slv.dev/bin/latest/shreds-rs',
   'shreds-ts': 'https://storage.slv.dev/bin/latest/shreds-ts',
 }
