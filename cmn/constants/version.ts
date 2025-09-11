a88952a50aac839f29c6ac537e980c4bf637d6e8
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-09-11 17:59:27 +0200

    update solana testnet version

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -5,11 +5,11 @@
 export const VERSION = '0.9.303'
 
 // Component versions
-export const VERSION_SOLANA_TESTNET = '3.0.0'
+export const VERSION_SOLANA_TESTNET = '3.0.1'
 export const VERSION_SOLANA_MAINNET = '2.3.8'
-export const VERSION_JITO_TESTNET = '3.0.0'
+export const VERSION_JITO_TESTNET = '3.0.1'
 export const VERSION_JITO_MAINNET = '2.3.8'
 export const VERSION_FIREDANCER_TESTNET = '0.708.20306'
-export const VERSION_FIREDANCER_MAINNET = '0.708.20306'
+export const VERSION_FIREDANCER_MAINNET = '0.709.30000'
 export const VERSION_RPC_SOLANA_JITO = '2.3.8'
 export const VERSION_GEYSER_YELLOWSTONE = 'v9.0.0+solana.2.3.8'
