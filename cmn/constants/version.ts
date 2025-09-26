6f1da91771502c70c3edeae2447b0bbff4ed51ec
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-09-27 00:12:06 +0200

    Update Solana Validator Testnet Version

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -5,9 +5,9 @@
 export const VERSION = '0.9.305'
 
 // Component versions
-export const VERSION_SOLANA_TESTNET = '3.0.3'
+export const VERSION_SOLANA_TESTNET = '3.0.4'
 export const VERSION_SOLANA_MAINNET = '2.3.10'
-export const VERSION_JITO_TESTNET = '3.0.3'
+export const VERSION_JITO_TESTNET = '3.0.4'
 export const VERSION_JITO_MAINNET = '2.3.10'
 export const VERSION_FIREDANCER_TESTNET = '0.709.30000'
 export const VERSION_FIREDANCER_MAINNET = '0.709.30000'
