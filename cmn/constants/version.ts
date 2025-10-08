256b6894429ccb4a29206287a821ad0b121848e6
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-10-08 16:54:09 +0200

    Release v0.9.506

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '0.9.505'
+export const VERSION = '0.9.506'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '2.3.11'
