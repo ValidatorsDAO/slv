83b8dd23d557ceafa2975264a7a4cfb752ab2230
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2025-12-04 12:25:37 +0100

    update solana testnet version

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -5,12 +5,12 @@
 export const VERSION = '0.9.805'
 
 // Component versions
-export const VERSION_SOLANA_TESTNET = '3.1.2'
-export const VERSION_SOLANA_DEVNET = '3.0.11'
-export const VERSION_SOLANA_MAINNET = '3.0.11'
-export const VERSION_JITO_TESTNET = '3.1.2-jito'
-export const VERSION_JITO_DEVNET = '3.0.11-jito'
-export const VERSION_JITO_MAINNET = '3.0.11-jito'
+export const VERSION_SOLANA_TESTNET = '3.1.4'
+export const VERSION_SOLANA_DEVNET = '3.0.12'
+export const VERSION_SOLANA_MAINNET = '3.0.12'
+export const VERSION_JITO_TESTNET = '3.1.4-jito'
+export const VERSION_JITO_DEVNET = '3.0.12-jito'
+export const VERSION_JITO_MAINNET = '3.0.12-jito'
 export const VERSION_FIREDANCER_TESTNET = '0.804.30008'
 export const VERSION_FIREDANCER_DEVNET = '0.804.30008'
 export const VERSION_FIREDANCER_MAINNET = '0.804.30008'
