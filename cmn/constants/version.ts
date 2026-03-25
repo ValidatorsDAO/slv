ff28dc4bbf81be97e72547ff50224820385eeeec
Sephiroth <sephiroth@elsoul.labo>
2026-03-25 16:22:02 +0000

    chore: bump version to v0.13.3

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '0.13.2'
+export const VERSION = '0.13.3'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '4.0.0-beta.2'
