79226873f23a1b6c674340465b27356893b501eb
セフィロス <sephiroth@elsoul.labo>
2026-04-02 20:51:43 +0000

    chore: bump version to 2026.4.2.2051

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '2026.4.2.1643'
+export const VERSION = '2026.4.2.2051'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '4.0.0-beta.2'
