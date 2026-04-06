0ba963ae18c83c13720ec76e0bfae6cde350012a
Sephiroth <sephiroth@elsoul.labo>
2026-04-06 09:15:54 +0000

    chore: release 2026.4.6.0912

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '2026.4.5.0441'
+export const VERSION = '2026.4.6.0912'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '4.0.0-beta.2'
