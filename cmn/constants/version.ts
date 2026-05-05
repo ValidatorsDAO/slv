b7fee97f48834b8c2532fb365b766ada261d5af1
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2026-05-05 16:13:16 +0200

    Release v2026.5.5.1612

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '2026.5.1.1510'
+export const VERSION = '2026.5.5.1612'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '4.0.0-beta.2'
