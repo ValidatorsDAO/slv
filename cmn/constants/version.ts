086e24923a002e7bb6e23a634cc1991d9e951f89
POPPIN-FUMI <23725836+POPPIN-FUMI@users.noreply.github.com>
2026-05-21 16:03:13 +0200

    Release v2026.5.21.1448 (#397)

--- cmn/constants/version.ts
+++ cmn/constants/version.ts
@@ -2,7 +2,7 @@
 // e.g.: VERSION + SOLANA CLI NAME + NETWORK = '0.0.1' only numbers and dots
 
 // SLV version
-export const VERSION = '2026.5.5.1612'
+export const VERSION = '2026.5.21.1448'
 
 // Component versions
 export const VERSION_SOLANA_TESTNET = '4.0.0-beta.2'
